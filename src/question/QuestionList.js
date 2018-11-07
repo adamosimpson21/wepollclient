5684df5c95e5eb9e83925a4c2e0fdaa21e8961ec
BandsWithLegends <adamosimpson21@gmail.com>
2018-11-07 13:04:50 -0800

    Styling question components

--- src/question/QuestionList.js
+++ src/question/QuestionList.js
@@ -10,12 +10,14 @@ class QuestionList extends Component{
     const { questions, currentUser } = this.props
     if(questions.length > 0){
       const allQuestions = questions.map(question => (
-        <div className='question-thumbnail' key={question._id} >
-          {(currentUser.isAuthenticated && currentUser.user.questions.includes(question._id)) && <div>Answered<Icon icon='success' viewBox='0 0 1028 1028' height='48px' width='48px'/></div>}
-          <p>{question.title.length>75 ? question.title.slice(0, 75).concat('...') : question.title}</p>
-          <p>{question.description.length>75 ? question.description.slice(0, 75).concat('...') : question.description}</p>
-          {(currentUser.isAuthenticated && currentUser.user.questions.includes(question._id)) ? <Link to={`/question/${question._id}/results`}><Button label='See current tally' /></Link>
-            : <Link to={'/question/'+question._id}><Button label='Check it out' /></Link>}
+        <div className='question-thumbnail-wrapper'>
+          <div className='question-thumbnail' key={question._id} >
+            {(currentUser.isAuthenticated && currentUser.user.questions.includes(question._id)) && <div>Answered<Icon icon='success' viewBox='0 0 1028 1028' height='48px' width='48px'/></div>}
+            <p>{question.title.length>75 ? question.title.slice(0, 75).concat('...') : question.title}</p>
+            <p>{question.description.length>75 ? question.description.slice(0, 75).concat('...') : question.description}</p>
+            {(currentUser.isAuthenticated && currentUser.user.questions.includes(question._id)) ? <Link to={`/question/${question._id}/results`}><Button label='See current tally' /></Link>
+              : <Link to={'/question/'+question._id}><Button label='Check it out' /></Link>}
+          </div>
         </div>
       ))
       return(<div className='question-list'>{allQuestions}</div>)
