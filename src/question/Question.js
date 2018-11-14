3d37d67adf79e741899a2107e4be71f3eaa6b8dd
BandsWithLegends <adamosimpson21@gmail.com>
2018-11-14 01:46:30 -0800

    Searching functioning and styled

--- src/question/Question.js
+++ src/question/Question.js
@@ -4,44 +4,17 @@ import {getAllQuestions} from '../store/actions/questions'
 import QuestionTitle from './QuestionTitle'
 import QuestionList from './QuestionList'
 import connect from 'react-redux/es/connect/connect'
-import {handleChange} from "../helper/handleChange";
-import Icon from "../hocs/Icon";
 
 class Question extends Component{
-  state={
-    searchText: ''
-  }
-
   componentDidMount(){
     this.props.getAllQuestions();
   }
 
-  filterQuestions = question => {
-    const searchText = this.state.searchText.toLowerCase()
-    return question.title.toLowerCase().includes(searchText) ||
-           question.description.toLowerCase().includes(searchText) ||
-           question.questionContent.toLowerCase().includes(searchText) ||
-           question.education.toLowerCase().includes(searchText) ||
-           question.answers.includes(searchText);
-  }
-
   render(){
     const { questions, currentUser } = this.props
-    const {searchText} = this.state
     return(<div>
       <QuestionTitle />
-      <div  className='question-search-bar'>
-      <Icon icon='search' viewBox='0 0 310.42 310.42'/>
-        <input
-          type='text'
-          name='searchText'
-          aria-label='Search for questions'
-          title='Search for questions'
-          value={this.state.searchText}
-          onChange = {handleChange.bind(this)}
-        />
-      </div>
-      <QuestionList questions={searchText.length > 2 ? questions.filter(this.filterQuestions) : questions} currentUser={currentUser} />
+      <QuestionList questions={questions} currentUser={currentUser} />
     </div>)
   }
 }
--- src/question/QuestionList.js
+++ src/question/QuestionList.js
@@ -4,9 +4,26 @@ import Link from 'react-router-dom/es/Link'
 import Button from "../hocs/Button";
 import Loader from "react-loader-spinner";
 import Icon from '../hocs/Icon'
+import {handleChange} from "../helper/handleChange";
 
 class QuestionList extends Component{
+  state={
+    searchText: ''
+  }
+
   sortByDate = (a,b) => new Date(a.createdAt) - new Date(b.createdAt);
+
+  filterQuestions = question => {
+    // TODO: naive approach, refine later
+    const searchText = this.state.searchText.toLowerCase()
+    if(searchText.length <= 1) return true;
+    return question.title.toLowerCase().includes(searchText) ||
+      question.description.toLowerCase().includes(searchText) ||
+      question.questionContent.toLowerCase().includes(searchText) ||
+      question.education.toLowerCase().includes(searchText) ||
+      question.answers.includes(searchText);
+  }
+
 
   firstQuestionFirst = (acc, element) => {
     if (element._id === '5bcd6e9fcef9fb37f8a72866' || element._id ==='5be3fbd45b0efc405424316c') {
@@ -15,33 +32,64 @@ class QuestionList extends Component{
     return [...acc, element];
   }
 
-  render(){
-    const { questions, currentUser } = this.props
+  questionPlacard = question => {
+    const { currentUser} = this.props
     const firstQId = process.env.REACT_APP_ENV_TYPE==='development' ? '5be3fbd45b0efc405424316c' : '5bcd6e9fcef9fb37f8a72866'
-
-    if(questions.length > 0){
-      const allQuestions = questions.sort(this.sortByDate).reduce(this.firstQuestionFirst, []).map(question => {
-        const hasAnswered = currentUser.isAuthenticated && currentUser.user.questions.includes(question._id)
-        return(<div className='question-thumbnail-wrapper' key={question._id}>
-          <div className={'question-thumbnail ' + (question._id === firstQId && !hasAnswered ? ' first-question-thumbnail' : '')}>
-            {hasAnswered && <div>Answered<Icon icon='success' viewBox='0 0 1028 1028' height='48px' width='48px'/></div>}
-            <p>{question.title.length > 75 ? question.title.slice(0, 75).concat('...') : question.title}</p>
-            <p>{question.description.length > 75 ? question.description.slice(0, 75).concat('...') : question.description}</p>
-            {hasAnswered ?
-              <Link to={`/question/${question._id}/results`}>
-                <Button label='See current tally'/>
-              </Link>
-              : <Link to={'/question/' + question._id}>
-                  <Button label='Check it out'/>
-                </Link>}
-          </div>
+    const hasAnswered = currentUser.isAuthenticated && currentUser.user.questions.includes(question._id)
+    return(<div className='question-thumbnail-wrapper' key={question._id}>
+        <div className={'question-thumbnail ' + (question._id === firstQId && !hasAnswered ? ' first-question-thumbnail' : '')}>
+          {hasAnswered && <div>Answered<Icon icon='success' viewBox='0 0 1028 1028' height='48px' width='48px'/></div>}
+          <p>{question.title.length > 75 ? question.title.slice(0, 75).concat('...') : question.title}</p>
+          <p>{question.description.length > 75 ? question.description.slice(0, 75).concat('...') : question.description}</p>
+          {hasAnswered ?
+            <Link to={`/question/${question._id}/results`}>
+              <Button label='See current tally'/>
+            </Link>
+            : <Link to={'/question/' + question._id}>
+              <Button label='Check it out'/>
+            </Link>}
         </div>
-        )})
-      return(<div className='question-list'>
-              {allQuestions}
-            </div>)
-    } else {
+      </div>
+    )
+  }
 
+
+  render(){
+    const { questions } = this.props
+    const searchBar = (<div  className='question-search-bar'>
+      <Icon icon='search' viewBox='0 0 310.42 310.42'/>
+      <input
+        type='text'
+        name='searchText'
+        aria-label='Search for questions'
+        title='Search for questions'
+        value={this.state.searchText}
+        onChange = {handleChange.bind(this)}
+      />
+    </div>)
+    if(questions.length > 0){
+      const allQuestions = questions.filter(this.filterQuestions)
+                                    .sort(this.sortByDate)
+                                    .reduce(this.firstQuestionFirst, [])
+                                    .map(this.questionPlacard)
+      if(allQuestions.length >= 1){
+        return(<div className='question-list-search-bar-wrapper'>
+          {searchBar}
+          <div className='question-list'>
+            {allQuestions}
+          </div>
+        </div>)
+      } else {
+        // Questions have loaded, but have been filtered out
+        return(<div className='question-list-search-bar-wrapper'>
+          {searchBar}
+          <div className='question-list-empty-set'>
+            <p>No polls matching that search</p>
+            <Link to="/newQuestionForm" className='question-new-form-link'><Button label={`Create a ${this.state.searchText} poll`}/></Link>
+          </div>
+        </div>)
+      }
+    } else {
       return(<div>
           <Loader
             type="Circles"
