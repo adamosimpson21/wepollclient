c525c1e41c0047ed254e993f27b1649cd0a472f3
BandsWithLegends <adamosimpson21@gmail.com>
2018-11-25 01:46:34 -0800

    refining css transitions for messages

--- src/hocs/MessageDisplay.js
+++ src/hocs/MessageDisplay.js
@@ -3,27 +3,31 @@ import './MessageDisplay.css'
 import connect from 'react-redux/es/connect/connect'
 import withRouter from 'react-router/es/withRouter'
 import {removeMessage} from "../store/actions/messages";
-import { CSSTransitionGroup } from 'react-transition-group'
 
 class MessageDisplay extends Component{
+  state={
+    showingMessage:true
+  }
+
+  fadeMessage = () => {
+    this.setState({showingMessage: false})
+    setTimeout(this.props.removeMessage, 2000);
+    setTimeout(() => this.setState({showingMessage: true}), 2001);
+  }
+
   render(){
     const { messages } = this.props
-    if(messages.length > 0){
+    const hasMessage = messages.length > 0
+    if(hasMessage){
       const allMessages = messages.map((message, index) => (
         <div className={`message-display message-${message.degree}-style` } key={index}>
           {message.message}
         </div>
       ))
-      setTimeout(this.props.removeMessage, 4000);
       return(
-        <div className='message-display-wrapper'>
-          <CSSTransitionGroup
-            transitionName="message-display-transition"
-            transitionEnterTimeout={500}
-            transitionLeaveTimeout={500}>
-          <button onClick={this.props.removeMessage} className='message-display-remove'><strong>X</strong></button>
+        <div className={`message-display-wrapper ${this.state.showingMessage ? 'message-showing' : 'message-hidden'}`}>
+          <button onClick={this.fadeMessage} className='message-display-remove'><strong>X</strong></button>
           {allMessages}
-          </CSSTransitionGroup>
         </div>
       )
     } else {
