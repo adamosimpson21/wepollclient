fb23b5e9e1a04b3369423511c165feebe03035b1
BandsWithLegends <adamosimpson21@gmail.com>
2018-12-06 23:16:22 -0800

    Adding more tests

--- src/tests/experience.test.js
+++ src/tests/experience.test.js
@@ -1,7 +1,13 @@
 import {MAX_LEVEL, xpToLevel} from '../helper/experience'
 
-test("experience function", () => {
-    for(let i = 1; i< MAX_LEVEL; i++ ){
-      expect(xpToLevel(i)).toBeGreaterThan(-1)
+const MAX_EXPERIENCE = 50000;
+
+test("experience function returns numbers", () => {
+    for(let i = 0; i< MAX_LEVEL; ++i ){
+      expect(xpToLevel(i)).toBeLessThan(MAX_EXPERIENCE+1)
     }
+})
+
+test("experience to level to experience matches", () => {
+  expect(true).toBe(true);
 })
