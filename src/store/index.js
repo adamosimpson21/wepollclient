e5c651594205a9a37cdacda030728da6356f5e74
BandsWithLegends <adamosimpson21@gmail.com>
2018-12-12 01:10:51 -0800

    Adding full devtools package

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,13 @@
 import rootReducer from "./reducers";
-import { createStore, applyMiddleware, compose } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from "redux-thunk";
 
 export function configureStore() {
-  const store = createStore(
+  return createStore(
     rootReducer,
     composeWithDevTools(
       applyMiddleware(thunk)
     )
   );
-
-  return store;
 }
