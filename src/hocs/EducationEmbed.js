27ce3f3809db279773e358484c4d5b7f8048e07e
BandsWithLegends <adamosimpson21@gmail.com>
2019-02-04 02:10:07 -0800

    Refactoring Education Embed without hasEmbed

--- src/hocs/EducationEmbed.js
+++ src/hocs/EducationEmbed.js
@@ -13,14 +13,12 @@ class EducationEmbed extends Component{
 
   render(){
     const {education} = this.props
-    const hasEmbed = isLink(education)
     const educationEmbed = (<div>{this.state.isRevealed ? [<Button label='Close Education' onClick={()=> this.setState({isRevealed:false})} key='close-education'/>,
                               <embed className='education-embed' src={formatUrlToEmbed(education)} key='education-embed'/>] :
                               <Button label='Learn more about this poll' onClick={()=> this.setState({isRevealed:true})}/>}
                             </div>)
-    const educationJSX = hasEmbed ? educationEmbed : (<span>{education}</span>)
-
-    return(<div className={'question-education' + (hasEmbed && ' hasEmbed')}>{educationJSX}</div>)
+    const educationJSX = isLink(education) ? educationEmbed : (<span>{education}</span>)
+    return(<div className='question-education'>{educationJSX}</div>)
   }
 }
 
