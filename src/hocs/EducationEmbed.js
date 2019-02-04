a6830bf2a56882059ce2ea2213bfa7dd101a4288
BandsWithLegends <adamosimpson21@gmail.com>
2019-02-03 23:38:09 -0800

    Making Question Education into it's own component

---
+++ src/hocs/EducationEmbed.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react;'
+import 'EducationEmbed.css';
+
+class EducationEmbed extends Component{
+  state={
+    isRevealed:false
+  }
+
+  render(){
+    return(<div />)
+  }
+}
+
+export default EducationEmbed;
